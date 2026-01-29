3f10a5689ec399be80e1f324c8fd67759596b466
pavan05-sai <pavansai050307@gmail.com>
2026-01-29 19:04:50 +0530

    Refactor app.py to improve readability and structure

--- app.py
+++ app.py
@@ -292,4 +292,4 @@ if __name__ == '__main__':
     if not os.path.exists(DB_PATH):
         from db_init import init_db
         init_db()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
